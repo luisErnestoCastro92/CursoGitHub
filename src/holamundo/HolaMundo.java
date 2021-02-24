8ea52b1b002df348ffbb43b26f8e5fb5d0a1dc22
unknown <luisErnestoCastro92>
2021-02-24 12:16:45 -0600

    se inicializaron las variables con valores desde el teclado

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -1,9 +1,23 @@
 package holamundo;
 
+import java.util.Scanner;
+
 public class HolaMundo {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int a, b, c;
+        System.out.println("ingresa el valor de a:");
+        a = sc.nextInt();
+        System.out.println("ingresa el valor de b:");
+        b = sc.nextInt();
+        System.out.println("ingresa el valor de c:");
+        c = sc.nextInt();
+        
+        System.out.println("valor de a: " + a);
+        System.out.println("valor de a: " + b);
+        System.out.println("valor de a: " + c);
+        
         System.out.println("hola mundo");
     }
 
